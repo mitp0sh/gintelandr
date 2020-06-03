63c28ed2143c75ebea6d086948d35c12194e52e4
mitp0sh <mitp0sh@mitp0sh.de>
2020-06-03 02:12:16 +0200

    Added support for run modes and started with project db support

--- src/main/java/bw/mitp0sh/gintelandr/GintelandrCommandLineParser.java
+++ src/main/java/bw/mitp0sh/gintelandr/GintelandrCommandLineParser.java
@@ -2,7 +2,9 @@ package bw.mitp0sh.gintelandr;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map.Entry;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -12,7 +14,11 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
-public class GintelandrCommandLineParser {	
+import com.google.common.flogger.FluentLogger;
+
+import bw.mitp0sh.gintelandr.GintelandrCommandLineParser.GintelandrRunMode;
+
+public class GintelandrCommandLineParser {
 	
 	protected static final Options OPTIONS = new Options();
 	
@@ -47,9 +53,45 @@ public class GintelandrCommandLineParser {
 	public GintelandrCommandLine parse(String[] arguments) throws ParseException {
 		return new GintelandrCommandLine(clp.parse(OPTIONS, arguments));
 	}
+	
+	public enum GintelandrRunMode {
+		UNDEFINED, INTERACTIVE, PROJECT, EXPORT;
+		
+		public String toString(){
+	        switch(this){
+        	case INTERACTIVE :
+        		return "INTERACTIVE";
+        	case PROJECT :
+        		return "PROJECT";
+        	case EXPORT :
+        		return "EXPORT";
+			default:
+				return "UNDEFINED";
+	        }
+	    }
+		
+		public static GintelandrRunMode toRunMode(String runMode) {
+			switch(runMode){
+    		case "INTERACTIVE" :
+    			return INTERACTIVE;
+    		case "PROJECT" :
+    			return PROJECT;	
+    		case "EXPORT" :
+    			return EXPORT;
+    		default:
+    			return UNDEFINED;
+			}
+		}
+		
+		public final boolean equals(String mode) {
+			return toRunMode(mode).equals(UNDEFINED) ? false : true; 
+		}		
+	} 
 }
 
 class GintelandrCommandLine{
+	
+	private static final FluentLogger flog = FluentLogger.forEnclosingClass();
 	
 	private static String LAST_ERROR = "";
 	
@@ -57,7 +99,7 @@ class GintelandrCommandLine{
 	private ArrayList<String> inputStrList = new ArrayList<>();	
 	private ArrayList<File> inputFileList = new ArrayList<>();
 	private String versionInformation = new Versioning().toString();
-	private ArrayList<String> modeList = new ArrayList<>();
+	private HashMap<String, GintelandrRunMode> modeList = new HashMap<>();
 
 	public GintelandrCommandLine(CommandLine cml) throws ParseException {
 		super();
@@ -75,12 +117,14 @@ class GintelandrCommandLine{
 			if(inputStrList.size() != 0 && inputFileList.size() == 0) {
 				Iterator<String> iter = inputStrList.iterator();
 				while(iter.hasNext()) {
+					String current = iter.next();
 					try {
-						File file = new File(iter.next());
+						File file = new File(current);
 						if(file.isDirectory() || file.isFile()) {
 							inputFileList.add(file);
 						}
 					} catch (Throwable t) {
+						flog.atWarning().log("invalid input source detected, ignoring...(input=\"" + current + "\")");
 						continue;
 					}
 				}
@@ -92,14 +136,19 @@ class GintelandrCommandLine{
 			String[] values = optValue.split("\\|");
 			if(values != null && values.length != 0) {
 				for (String v : values) {
-					modeList.add(v.trim());
+					GintelandrRunMode runMode = GintelandrRunMode.toRunMode(v);
+					if(!runMode.equals(GintelandrRunMode.UNDEFINED)) {
+						modeList.put(v, runMode);
+					}
 				}
 			}
 		}
 		
 		// final step - validate commandLineParameter
 		if(!validateCommandLine()) {
-			throw new ParseException("Validation error - " + LAST_ERROR);
+			String msg = "validation error occured, aborting...(last error=\"" + LAST_ERROR + "\")";
+			flog.atSevere().log(msg);
+			throw new ParseException(msg);
 		}
 	}	
 	
@@ -136,21 +185,20 @@ class GintelandrCommandLine{
 	}
 	
 	public boolean hasMode(String mode) {
-		for(String m : modeList) {
-			if(m.equals(mode)) {
-				return true;
-			}
-		}
-		
-		return false;
+		return modeList.get(mode) != null ? true : false;
 	} 
+	
+	public HashMap<String, GintelandrRunMode> getModes() {
+		return modeList;
+	}
 	
 	public String getVersionInformation() {
 		return versionInformation;
 	}
 	
 	private boolean validateCommandLine() {		
-		if(isVersion()) {
+		/* defaults which do nothing functional */
+		if(isVersion() || isHelp()) {
 			return true;
 		}
 		
@@ -159,6 +207,22 @@ class GintelandrCommandLine{
 			return false;
 		}
 		
+		/* check if all passed modes are valid */
+		if(modeList.size() != 0) {
+			Iterator<Entry<String, GintelandrRunMode>> runModeIter = modeList.entrySet().iterator();
+			while(runModeIter.hasNext()) {
+				Entry<String, GintelandrRunMode> current = runModeIter.next();
+				GintelandrRunMode value = current.getValue();
+				if(value.equals(GintelandrRunMode.UNDEFINED)) {
+					LAST_ERROR = "GintelandrRunMode.UNDEFINED not allowed";
+					return false;
+				}
+			}
+		}
+		
+		LAST_ERROR = "";
 		return true;
-	}
+	}	
 }
+
+
--- src/main/java/bw/mitp0sh/gintelandr/GintelandrConsole.java
+++ src/main/java/bw/mitp0sh/gintelandr/GintelandrConsole.java
@@ -1,10 +1,13 @@
 package bw.mitp0sh.gintelandr;
 
 import java.io.File;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 import com.google.common.flogger.FluentLogger;
+
+import bw.mitp0sh.gintelandr.GintelandrCommandLineParser.GintelandrRunMode;
 
 public class GintelandrConsole {
 	
@@ -12,6 +15,18 @@ public class GintelandrConsole {
 	protected static GintelandrCommandLine cml = null;
 
 	public static void main(String[] args) {
+		flog.atInfo().log("Gintelandr is warming-up! Please wait...");
+		
+		/* register shutdown hook for required clean-up */
+		Runtime.getRuntime().addShutdownHook(new Thread() 
+	    { 	
+			public void run() {	    		    	
+			  flog.atInfo().log("shutdown hook entered. Starting clean-up now...");
+			  DatabaseHelper.closeTmpDB();			  
+			} 
+	    });
+		
+		/* start commandline parsing */
 		GintelandrCommandLineParser commandLineParser = new GintelandrCommandLineParser();
 		try {
 			cml = commandLineParser.parse(args);
@@ -35,6 +50,17 @@ public class GintelandrConsole {
 		if(!cml.isInput()) { // at least one input needs to be specified
 			GintelandrCommandLine.printHelp(null);
 			return;
+		}
+		
+		if(!cml.isMode()) { // mode is optional and if not specified default to INTERACTIVE
+			cml.getModes().put(GintelandrRunMode.INTERACTIVE.toString(), GintelandrRunMode.INTERACTIVE);
+		}
+		
+		try {
+			DatabaseHelper.openTmpDB();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 		/* this is the main entry point into analysis - let the game begin */
